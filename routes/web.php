93e79011560cb7ba41e49ea3a1dae936c9a9ca74
ferdy1807 <ferdy.perdana@yahoo.com>
2019-09-19 16:00:23 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product/display', ProductController@showAll);
+Route::post('/product/save', ProductController@saveNew);
